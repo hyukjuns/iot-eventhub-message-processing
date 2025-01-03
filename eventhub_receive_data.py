cf712b9d10ea646aa279bf5488e3fe7e78f168c5
hyukjun <hyukjuner@gmail.com>
2025-01-03 17:50:44 +0900

    update sample scripts

---
+++ eventhub_receive_data.py
@@ -0,0 +1,54 @@
+import os
+import asyncio
+
+from azure.eventhub.aio import EventHubConsumerClient
+from azure.eventhub.extensions.checkpointstoreblobaio import (
+    BlobCheckpointStore,
+)
+from azure.identity.aio import DefaultAzureCredential
+
+BLOB_STORAGE_CONNECTION_STRING = os.getenv("BLOB_STORAGE_CONNECTION_STRING ")
+BLOB_CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME")
+EVENT_HUB_CONNECTION_STR = os.getenv("EVENT_HUB_CONNECTION_STR ")
+EVENT_HUB_NAME = os.getenv("EVENT_HUB_NAME")
+
+credential = DefaultAzureCredential()
+
+async def on_event(partition_context, event):
+    # Print the event data.
+    print(
+        'Received the event: "{}" from the partition with ID: "{}"'.format(
+            event.body_as_str(encoding="UTF-8"), partition_context.partition_id
+        )
+    )
+
+    # Update the checkpoint so that the program doesn't read the events
+    # that it has already read when you run it next time.
+    await partition_context.update_checkpoint(event)
+
+
+async def main():
+    # Create an Azure blob checkpoint store to store the checkpoints.
+    checkpoint_store = BlobCheckpointStore(
+        BLOB_STORAGE_CONNECTION_STRING, BLOB_CONTAINER_NAME
+    )
+
+    # Create a consumer client for the event hub.
+    client = EventHubConsumerClient(
+        EVENT_HUB_CONNECTION_STR,
+        eventhub_name=EVENT_HUB_NAME,
+        consumer_group="$Default",
+        checkpoint_store=checkpoint_store
+    )
+    async with client:
+        # Call the receive method. Read from the beginning of the partition
+        # (starting_position: "-1")
+        await client.receive(on_event=on_event, starting_position="-1")
+
+    # Close credential when no longer needed.
+    await credential.close()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    # Run the main method.
+    loop.run_until_complete(main())
